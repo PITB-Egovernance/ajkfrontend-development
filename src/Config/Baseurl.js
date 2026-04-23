5d0af3d7a3a19645cdb5e61a0c39be07515cfedb
Ismail Ahmad <107646577+hafizismail608@users.noreply.github.com>
2026-04-23 14:41:40 +0500

    live backend with frontend

--- src/Config/Baseurl.js
+++ src/Config/Baseurl.js
@@ -1,7 +1,7 @@
 const productionUrl = "https://api-admin-ajkpsc.punjab.gov.pk/api/v1";
 const localUrl      = "http://localhost:3000";
-// const apiUrl        = "https://api-admin-ajkpsc.punjab.gov.pk/api/v1";    
-const apiUrl        = "http://localhost:8000/api/v1";
+const apiUrl        = "https://api-admin-ajkpsc.punjab.gov.pk/api/v1";    
+// const apiUrl        = "http://localhost:8000/api/v1";
 const apiKey        = "9kX7pL2mQ8rT5vY3nZ6bJ1hF4gD0eA9cU8iO2sV7tE5rW";
 
 
