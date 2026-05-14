cdd34000309bd1296e37e14052b32af5e8b41352
Farhan Saqib <farhansaqib67@gmail.com>
2026-05-14 18:49:11 +0500

    chore: cleanup environment files and update gitignore

--- fix_urls.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path "src" -Recurse -Include *.jsx,*.js
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName
-    if ($content -match 'Config.productionUrl') {
-        $newContent = $content -replace 'Config.productionUrl', 'Config.apiUrl'
-        $newContent | Set-Content $file.FullName
-        Write-Host "Updated: $($file.FullName)"
-    }
-}
