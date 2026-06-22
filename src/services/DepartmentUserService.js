438cae84650ff65850e27464d8cb957b26e94804
ayazkhalid9394 <ak46bs@gmail.com>
2026-06-22 17:40:30 +0500

    change department service add update and delete correct routes
    change department service add update and delete correct routes

--- src/services/DepartmentUserService.js
+++ src/services/DepartmentUserService.js
@@ -99,7 +99,7 @@ class DepartmentUserService {
   // }
 
   static async update(hashId, payload) {
-    const response = await fetch(`${API_BASE}/department-users/${hashId}`, {
+    const response = await fetch(`${API_BASE}/department-users/update/${hashId}`, {
       method: 'PUT',
       headers: {
         Authorization: `Bearer ${AuthService.getToken()}`,
@@ -121,7 +121,7 @@ class DepartmentUserService {
 
 
   static async delete(id) {
-    const response = await fetch(`${API_BASE}/department-users/${id}`, {
+    const response = await fetch(`${API_BASE}/department-users/${id}/delete`, {
       method: 'DELETE',
       headers: getHeaders(false),
     });
